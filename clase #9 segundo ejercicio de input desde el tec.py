daad71cf49d902128a7ffef89a95466b470629a9
Samrai11 <121623430+Samrai11@users.noreply.github.com>
2024-09-07 01:05:53 +0000

    Pending changes exported from your codespace

---
+++ clase #9 segundo ejercicio de input desde el tec.py
@@ -0,0 +1,7 @@
+#segundo ejercicio de input desde el teclado
+nombre= input("¿Cual es tu nombre?: ")
+print("hola " + nombre, " vamos a realizar una suma ")
+num_uno = int(input(" por favor introduce el primer valor: "))
+num_dos = int(input(" por favor introduce el segundo valor: "))
+resultado = num_uno + num_dos
+print(nombre + " el resultado de la suma es: ", (resultado))
