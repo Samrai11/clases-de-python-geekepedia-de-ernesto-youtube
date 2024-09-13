fb6e8b562a54dccf0dec7dcf7608e294186a09e7
Samrai11 <121623430+Samrai11@users.noreply.github.com>
2024-09-13 17:37:42 +0000

    nueva clase 11

---
+++ numero_uno = 5.py
@@ -0,0 +1,14 @@
+numero_uno = 5
+if numero_uno == 5:
+  print("el numero es cinco")
+else:
+  print("el nunero no es cinco")
+print("Fin.")
+
+# cuando no se  cumple la condición se pasa elses(entonces), da otro resultado condicional
+numero_uno = 10
+if numero_uno == 5:
+  print("el numero es cinco")
+else:
+  print("el nunero no es cinco")
+print("Fin.")
