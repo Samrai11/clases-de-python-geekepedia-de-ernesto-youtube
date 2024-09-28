cd86b8e0db21a56f07cb65e28271888f56dfc60c
Samrai11 <121623430+Samrai11@users.noreply.github.com>
2024-09-28 01:10:13 +0000

    terminada de clase 27 f string

--- clase #27 concatenacion con f-string.py
+++ clase #27 concatenacion con f-string.py
@@ -5,4 +5,9 @@ print( f"el resultado de la operacion 4 + 1 es = {4+1}")
 nombre = "carlos"
 edad = 22
 estatura = 1.8
-print(f"hola {nombre}")
+print(f"hola {nombre} tienes {edad} años y mides {estatura} metros.")
+#ejemplo 3
+nombre = input ("¿cual es tu nombre?")
+num_uno= int(input("introducce un numero: "))
+num_dos= int(input("introduce un segundo numero: "))
+print(f"hola {nombre} el resultado de {num_uno} + {num_dos} es: {num_uno + num_dos}")
