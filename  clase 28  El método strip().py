9d2b6a06baeaff5abc66b08ba8eb96c17a321ea2
Samrai11 <121623430+Samrai11@users.noreply.github.com>
2024-10-25 23:43:01 +0000

    clase 28 lista

---
+++  clase 28  El método strip().py
@@ -0,0 +1,15 @@
+cadena= " hola ernesto " 
+cadena= cadena.strip( )
+print (cadena)
+
+cadena= " hola ernesto " 
+cadena= cadena.strip("s tHo")
+print (cadena)
+
+
+cadena= "\t hola hernesto\n"
+print(cadena)
+cadena= cadena.strip("s tHo \t\n" )
+print (cadena)
+
+
