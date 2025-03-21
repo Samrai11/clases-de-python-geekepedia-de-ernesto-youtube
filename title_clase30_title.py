6e8ca58d104d9500a002ed3ecc4cefd2bc73f241
Samrai11 <121623430+Samrai11@users.noreply.github.com>
2025-03-21 23:06:46 +0000

    falta

---
+++ title_clase30_title.py
@@ -0,0 +1,6 @@
+firt_name= input( "nombre: ")
+last_name= input("Apellido: ")
+full_name= f"(firt_name) (last_name)"
+print (f"¿el metodo title() se ha aplicado correctamente?: {full_name.istitle ()}")
+print ( f"Aplicando el metodo title (): {full_name.title() }")
+print(f"volvemos a imprimir el nombre: {full_name}")
