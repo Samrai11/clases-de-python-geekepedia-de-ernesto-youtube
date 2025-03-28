cbfa9a8893ba2e0bded6e1d2fb410680045f2dcd
Samrai11 <121623430+Samrai11@users.noreply.github.com>
2025-03-28 23:36:52 +0000

    clase 31 islower, isupper

---
+++ clase 31metodos islower,lower,isupper,upper.py
@@ -0,0 +1,8 @@
+variable= "La Geekeepedia"
+print (variable.islower() )
+variable= variable.lower()
+print(variable)
+print(variable.isupper())
+print(variable.upper())
+print( " ")
+print(variable)
